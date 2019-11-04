a0d441e7984b87443d87488688b9fb62535f5b04
lucafedri <9001053+fedriz@users.noreply.github.com>
2019-11-04 15:57:32 +0100

    Add edit to beacon list

--- src/reducers/beaconsReducer.js
+++ src/reducers/beaconsReducer.js
@@ -1,7 +1,9 @@
 export const beaconsReducerActionTypes = {
   GET_BEACON_LIST_SUCCESS: "GET_BEACON_LIST_SUCCESS",
   CREATE_BEACON_SUCCESS: "CREATE_BEACON_SUCCESS",
-  DELETE_BEACON_SUCCESS: "DELETE_BEACON_SUCCESS"
+  DELETE_BEACON_SUCCESS: "DELETE_BEACON_SUCCESS",
+  SELECT_CURRENT_BEACON: "SELECT_CURRENT_BEACON",
+  EDIT_BEACON_SUCCESS: "EDIT_BEACON_SUCCESS"
 };
 
 const INITIAL_STATE = {
@@ -15,15 +17,34 @@ export const beaconsReducer = (state = INITIAL_STATE, action) => {
         ...state,
         beaconList: { ...action.payload }
       };
+
     case beaconsReducerActionTypes.GET_BEACON_LIST_SUCCESS:
       return {
         ...state,
         beaconList: { ...action.payload }
       };
+
     case beaconsReducerActionTypes.DELETE_BEACON_SUCCESS:
       return {
         ...state,
         beaconList: { ...action.payload }
+      };
+
+    case beaconsReducerActionTypes.EDIT_BEACON_SUCCESS:
+      return {
+        ...state,
+        beaconList: { ...action.payload }
+      };
+
+    case beaconsReducerActionTypes.SELECT_CURRENT_BEACON:
+      const currentBeacon = state.beaconList.results.find(
+        o => o.id === action.payload
+      );
+
+      return {
+        ...state,
+        currentBeacon,
+        currentBeaconId: action.payload
       };
 
     default:
---
+++ src/routes/beacon/components/editBeaconForm.js
@@ -0,0 +1,46 @@
+import { html } from "lit-html";
+import { editBeaconAction } from "../../../actions/beaconsActions";
+import { store } from "../../../createStore";
+
+export function editBeaconForm(state) {
+  const { currentBeacon } = state.beaconsReducer;
+
+  let newBeaconName = currentBeacon.name;
+  let newBeaconId = currentBeacon.beacon_id;
+
+  return html`
+    <form
+      action=""
+      @submit=${e => {
+        e.preventDefault();
+        store.dispatch(
+          editBeaconAction(currentBeacon.id, {
+            name: newBeaconName,
+            beacon_id: newBeaconId
+          })
+        );
+      }}
+    >
+      <label>Name</label>
+      <input
+        value=${newBeaconName}
+        @change=${e => {
+          newBeaconName = e.target.value;
+        }}
+        type="text"
+      />
+
+      <label>Id</label>
+      <input
+        value=${newBeaconId}
+        @change=${e => {
+          newBeaconId = e.target.value;
+        }}
+        type="text"
+      />
+      <button submit class="full_width submit">
+        Edit
+      </button>
+    </form>
+  `;
+}
