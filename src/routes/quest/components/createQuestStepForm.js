c3eb9fd131336080b050f04e36bfa4ea764b52f5
Nicolò Pretto <nicolo@belkadigital.com>
2019-12-11 10:56:45 +0100

    removed type in step creation

--- src/routes/quest/components/createQuestStepForm.js
+++ src/routes/quest/components/createQuestStepForm.js
@@ -27,7 +27,6 @@ export function createQuestStepForm(state) {
           newQuestStepValue_points &&
           newQuestStepValue_points_error &&
           newQuestStepQuest_index &&
-          newQuestStepType &&
           newQuestStepInstructions &&
           newQuestStepBeacon
         ) {
@@ -38,7 +37,6 @@ export function createQuestStepForm(state) {
               value_points: newQuestStepValue_points,
               value_points_error: newQuestStepValue_points_error,
               quest_index: newQuestStepQuest_index,
-              type: newQuestStepType,
               instructions: newQuestStepInstructions,
               quest: currentQuest.id,
               beacon: newQuestStepBeacon,
@@ -57,15 +55,6 @@ export function createQuestStepForm(state) {
           newQuestStepName = e.target.value;
         }}
         name="name"
-      />
-
-      <label>Type</label>
-      <input
-        value=${newQuestStepType}
-        @keyup=${e => {
-          newQuestStepType = e.target.value;
-        }}
-        name="type"
       />
 
       <label>Instructions <small>text of the step</small></label>
