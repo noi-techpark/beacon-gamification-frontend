00094975d5b2f1ea332b8b7dcfa7ab3351555a5e
Nicolò Pretto <nicolo@belkadigital.com>
2019-12-12 10:30:06 +0100

    BST-130 added footer

--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
-import { html, LitElement } from "lit-element";
+import { LitElement, html, css } from "lit-element";
 import "../src/routes/login";
 import "../src/routes/quest/questRoute";
 import "../src/routes/beacon/beaconRoute";
 import "./components/modal";
 import "./components/navbar";
+import "./components/footer";
 import "./components/private-route";
 
 class Root extends LitElement {
+  constructor() {
+    super();
+    this.current_route = "";
+  }
+
   static get properties() {
     return {
       current_route: { type: String }
     };
   }
-  constructor() {
-    super();
-    this.current_route = "";
+
+  static get styles() {
+    return css`
+      :host {
+        display: flex;
+        height: 100vh;
+        flex-direction: column;
+      }
+      .content {
+        flex: 1;
+        overflow: scroll;
+      }
+    `;
   }
 
   firstUpdated() {
@@ -28,28 +44,31 @@ class Root extends LitElement {
             <x-navbar></x-navbar>
           `
         : null}
-      ${this.current_route === "/"
-        ? html`
-            <h2 style="margin-left: 2rem;">
-              Hi, welcome to the quest creator.
-            </h2>
-          `
-        : null}
-      ${this.current_route === "/login"
-        ? html`
-            <x-route-login></x-route-login>
-          `
-        : null}
-      ${this.current_route === "/quests"
-        ? html`
-            <private-route><x-route-quests></x-route-quests></private-route>
-          `
-        : null}
-      ${this.current_route === "/beacons"
-        ? html`
-            <private-route><x-route-beacon></x-route-beacon></private-route>
-          `
-        : null}
+      <div class="content">
+        ${this.current_route === "/"
+          ? html`
+              <h2 style="margin-left: 2rem;">
+                Hi, welcome to the quest creator.
+              </h2>
+            `
+          : null}
+        ${this.current_route === "/login"
+          ? html`
+              <x-route-login></x-route-login>
+            `
+          : null}
+        ${this.current_route === "/quests"
+          ? html`
+              <private-route><x-route-quests></x-route-quests></private-route>
+            `
+          : null}
+        ${this.current_route === "/beacons"
+          ? html`
+              <private-route><x-route-beacon></x-route-beacon></private-route>
+            `
+          : null}
+      </div>
+      <x-footer> </x-footer>
     `;
   }
 }
