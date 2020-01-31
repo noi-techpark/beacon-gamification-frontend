be72d877f1a5511621febf375176ef026f35d48d
fedriz <9001053+fedriz@users.noreply.github.com>
2020-01-31 11:51:09 +0100

    Add custom env solution

---
+++ env.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+const env = process.env.uenv;
+
+fs.readFile("../prodApiUrl.txt", "utf8", (err, url) => {
+  if (err) throw err;
+  fs.writeFileSync(
+    "./environments/current.js",
+    `export const baseApiPath = "${
+      env === "prod" ? url : "http://127.0.0.1:8000/api/v1"
+    }";
+export const env = "${env}";`,
+    "utf8"
+  );
+});
