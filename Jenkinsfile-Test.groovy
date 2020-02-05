bf407dc43eda09b70a757fd58df4129c9a713e07
Alex Lanz <alex@krumer.it>
2020-02-05 10:26:15 +0100

    Deployment

---
+++ Jenkinsfile-Test.groovy
@@ -0,0 +1,38 @@
+pipeline {
+    agent {
+        dockerfile {
+            filename 'docker/dockerfile-node'
+            additionalBuildArgs '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
+        }
+    }
+
+    environment {
+        AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
+        AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
+
+        API = "https://api.gamification.beacon.testingmachine.eu/api/v1"
+    }
+
+    stages {
+        stage('Configure') {
+            steps {
+                sh "echo '${API}' > config/api-url.txt"
+            }
+        }
+        stage('Dependencies') {
+            steps {
+                sh 'yarn install'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'yarn run pb'
+            }
+        }
+        stage('Upload') {
+            steps {
+                s3Upload(bucket: 'test-gamification-web', acl: 'PublicRead', file: './es5prod')
+            }
+        }
+    }
+}
